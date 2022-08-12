e461a96e0da106339b92698ee5f037dc54249050
unknown <ranveer1990@hotmail.com>
2022-08-12 11:28:22 +0400

    creating get to filter via id

---
+++ src/routes/getListing.js
@@ -0,0 +1,11 @@
+import { fakeListings } from "./fake-data";
+
+export const getListingRoute = {
+        //getting the data by id
+        method: 'GET',
+        path: '/api/listings/{id}',
+        handler: (req, h) => {
+            const id = req.params.id;
+            return fakeListings.find(listing => listing.id === id)
+        }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 import { getAllListingsRoute } from "./getAllListings";
+import { getListingRoute } from "./getListing";
 
 export default [
+    //inserting the variable to export to the server
     getAllListingsRoute,
+    getListingRoute
 ]
