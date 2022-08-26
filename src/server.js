806be455ce0df9cea272a721f759cb0df1125ad6
unknown <ranveer1990@hotmail.com>
2022-08-26 15:04:48 +0400

    firebase remove

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import Hapi, { server } from '@hapi/hapi';
 import routes from './routes';
 import { db } from './database';
-import * as admin from 'firebase-admin'
+/* import * as admin from 'firebase-admin'
 import credentials from '../credentials.json'
-
-admin.initializeApp({
+ */
+/* admin.initializeApp({
     credential: admin.credential.cert(credentials)
 });
-
+ */
 
 const start = async() => {
     //creating server: host and port
